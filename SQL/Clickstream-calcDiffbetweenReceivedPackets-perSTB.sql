5f6a528900dd8bf4261150d32fa22d93d5111073
gevgev <gevgev@yahoo.com>
2016-05-22 11:30:10 -0700

    diff script for received

---
+++ SQL/Clickstream-calcDiffbetweenReceivedPackets-perSTB.sql
@@ -0,0 +1,24 @@
+--CTE - ALL 
+With tblDifference as
+(
+Select Row_Number() OVER (Order by [deviceId], [timestamp]) as RowNumber, [deviceId], [timestamp] from 
+	( SELECT DISTINCT deviceid, received as [timestamp]
+	  FROM  dbo.clickstreamEventsLog
+	  WHERE msoName = 'Click-Tacoma'
+	-- dbo.Packets
+	) Packets
+)
+--Actual Query
+Select ' ' +Cur.[deviceId],
+	Cur.[timestamp] as [Current], 
+	Prv.[timestamp] as [Previous],
+	convert(varchar, datediff (s, Prv.[timestamp], Cur.[timestamp]) / (60 * 60 * 24)) + ' : '
+	+ convert(varchar, dateadd(s, datediff (s, Prv.[timestamp], Cur.[timestamp]), convert(datetime2, '0001-01-01')), 108) as [Difference]
+--	 Cur.[timestamp] - Prv.[timestamp] as [Difference] 
+from tblDifference Cur Left Outer Join tblDifference Prv
+On Cur.RowNumber=Prv.RowNumber+1
+WHERE Cur.[deviceId] = Prv.[deviceId]
+--	AND (datediff (s, Prv.[timestamp], Cur.[timestamp]) > (6 * 60 * 60)) -- = seconds in 6 hours = 6h * 60 mins * 60 seconds
+Order by Cur.[deviceId], Cur.[timestamp]
+
+GO
