0fdfbf28302ae189a84e7d2f13f856999132a3a0
gevgev <gevgev@yahoo.com>
2016-05-24 01:05:35 -0700

    centos docker: fully functional/tested terraform-docker-script -> ftp, unzip, ->csv, ->s3

--- centos/run-ftp-s3.sh
+++ centos/run-ftp-s3.sh
@@ -6,6 +6,8 @@ ftppath=$1
 file=$2
 bucket=$3
 
+readonly tmpfolder="temp-r31"
+
 #ftp -in ec2-52-24-138-152.us-west-2.compute.amazonaws.com << SCRIPTEND
 #user adsawsftp quickaccess123
 #binary
@@ -19,19 +21,19 @@ curl -u  adsawsftp:quickaccess123 'ftp://ec2-52-24-138-152.us-west-2.compute.ama
 
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-echo `mkdir tmp`
-echo `unzip "$file" -d "tmp"`
+echo `mkdir "$tmpfolder"`
+echo `unzip "$file" -d "$tmpfolder"`
 
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-./csbufferanalizer -L -M 50000 -d "tmp"
+./csbufferanalizer -L -M 50000 -d "$tmpfolder"
 
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-echo `rm -f tmp/*`
-echo `mv *.csv tmp/`
+echo `rm -f "$tmpfolder"/*`
+echo `mv *.csv "$tmpfolder"/`
 
-aws s3 cp tmp/ s3://"$bucket" --recursive
+aws s3 cp "$tmpfolder"/ s3://"$bucket" --recursive
 
 #echo `rm -f tmp/*`
 #echo `rm "$file"`
